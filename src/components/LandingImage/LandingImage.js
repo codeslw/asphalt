91dbdd9d58f29814d0e94b5120e7af48fcb3767b
Saidkamol Anvarkhujaev <saidkamolanvarkhujaev@Saidkamols-MacBook-Pro.local>
2021-03-12 05:19:28 +0500

    LandingImage

--- src/components/LandingImage/LandingImage.js
+++ src/components/LandingImage/LandingImage.js
@@ -7,14 +7,14 @@ const LandingImage = () => {
         <div className = {classes.LandingImage}>
             <div className={classes.content}>
                 <div className={classes.text}>
-                    <h1> Some text to hook customers</h1>
+                    <h1> Some text to hook customers and let us know what they want...</h1>
                 </div>
                 <div className={classes.button}>
                     <button> Lets Start!</button>
                 </div>
             </div>
             <div className={classes.image}>
-                
+                <img src="https://constructionequipment.com/sites/ce/files/styles/list/public/Bomag-BW-219-single-drum-compactor.gif?itok=OCoy1s-B" alt=""/>
             </div>
         </div>
 
